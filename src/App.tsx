4fb19217deeebf323428e28584b637c196f5a8b4
taiyou <hiromacha1116@icloud.com>
2023-10-09 11:27:44 +0900

    フロントに初期設定の見た目作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
 
+import { useState } from "react";
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
+  const [dbId, setDbId] = useState("");
+  const [key, setKey] = useState("");
+
+  const saveKey = () => {
+    
+  }
+
+  const saveDbId = () => {
+
+  }
+
+
   return(
-    <div>
-      hello
+    <div className="flex flex-col items-center justify-center h-screen gap-5">
+      <h1 className="font-bold text-2xl">初期設定</h1>
+      <div className="flex gap-3 items-start">
+        <input 
+          type="text" 
+          placeholder="シークレットキーを入力" 
+          className="px-4 py-2 border rounded-md"
+          onChange={(e) => setKey(e.target.value)}
+        />
+        <Button 
+          text="シークレットキー保存"
+          variant="primary"
+          onClick={() => saveKey()}
+        />
+      </div>
+      <div className="flex gap-3 items-start">
+        <input 
+          type="text" 
+          placeholder="データベースIDを入力" 
+          className="px-4 py-2 border rounded-md"
+          onChange={(e) => setDbId(e.target.value)}
+        />
+        <Button 
+          text="データベースIDを保存"
+          variant="primary"
+          onClick={() => saveDbId()}
+        />
+      </div>
     </div>
   )
 }
