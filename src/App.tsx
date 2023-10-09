5f1649f3f78bd2a32f36da6ecbb5e736d562c8c0
taiyou <hiromacha1116@icloud.com>
2023-10-09 14:37:40 +0900

    enterで出力

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   const [dbId, setDbId] = useState("");
   const [secretKey, setSecretKey] = useState("");
   const [isSetting, setIsSetting] = useState(false);
+  const [copyText, setCopyText] = useState("");
 
   useEffect(() => {
     const initialize = async () => {
@@ -30,13 +31,36 @@ function App() {
     }
     setIsSetting(true);
   }
+
+  const handleKeyDown = (e: any) => {
+    if (e.key === 'Enter') {
+      sendReqwestToNotion(e);
+    }
+  };
+
+  const sendReqwestToNotion = async (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    e.preventDefault();
+    console.log("Before setCopyText: ", copyText);
+    setCopyText("");
+  };
   
   return(
-    <div>
+    <div className="bg-gray-200">
       { isSetting ? (
-        <h1>北</h1>
+        <div className="flex flex-col h-screen items-center justify-center">
+          <ul className="m-3">
+            <li><p>①コピーしたテキストをペースト(cmd + v)</p></li>
+            <li><p>②Enter</p></li>
+          </ul>
+            <textarea 
+              className="border h-[66vh] w-[66vw] resize-none rounded-md py-2 px-2"
+              onChange={(e) => setCopyText(e.target.value)}
+              value={copyText}
+              onKeyDown={(e) => handleKeyDown(e)}
+            />
+        </div>
       ) : (
-        <div className="flex flex-col items-center justify-center h-screen gap-5 bg-gray-200">
+        <div className="flex flex-col items-center justify-center h-screen gap-5">
           <h1 className="font-bold text-2xl">初期設定</h1>
           <div className="flex gap-3">
             <input 
